28ea05d2870db6c8b953bfef42d4f440003a8bb2
J. Ryan Conklin <jryanconklin@gmail.com>
2020-03-11 13:26:55 -0700

    First pass at nestable grid component

--- apps/node-pl/pattern-lab/_patterns/01-atoms-demo/grid/index.js
+++ apps/node-pl/pattern-lab/_patterns/01-atoms-demo/grid/index.js
@@ -1,2 +1,13 @@
-import './grid-demo.twig';
-import './layout-demo.twig';
+/**
+ * Demo of grid. Pulls in grid assets, and provides demo-only assets.
+ *
+ * (This file is NOT imported by the design system, but is included as part of
+ * a Pattern Lab app.)
+ */
+
+// Import component assets
+import 'atoms/grid';
+
+// Import demo assets
+import './grids.twig';
+import './grids.yml';
